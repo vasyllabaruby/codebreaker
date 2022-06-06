78fb54ff0c4bf1d0d4db5966875e15403ee33ac1
Vasyl <vasyl.laba@rubygarage.org>
2022-06-06 16:03:44 +0300

    Create secret code entity

---
+++ lib/codebreaker/secret_code.rb
@@ -0,0 +1,18 @@
+module Codebreaker
+  class SecretCode
+    CODE_SIZE = 4
+    attr_reader :s_code
+
+    def initialize(s_code = "")
+      @s_code = s_code
+    end
+
+    def generate
+      CODE_SIZE.times { @s_code += rand(1..6).to_s }
+    end
+
+    def get_code
+      @s_code
+    end
+  end
+end
