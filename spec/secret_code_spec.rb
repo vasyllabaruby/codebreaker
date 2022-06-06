30fe9c6860d6cbdcc55a2d19d0ee16f6a765737e
Vasyl <vasyl.laba@rubygarage.org>
2022-06-06 23:58:18 +0300

    Add test for sercet code entity

---
+++ spec/secret_code_spec.rb
@@ -0,0 +1,24 @@
+
+
+RSpec.describe 'SecretCode' do
+  let(:code) { Codebreaker::SecretCode.new('1111') }
+
+  context '#initialize' do
+    it 'initialize instant' do
+      expect(code).to be_instance_of(Codebreaker::SecretCode)
+    end
+  end
+
+  context '#get' do
+    it 'get code' do
+      expect(code.get).to eq('1111')
+    end
+  end
+
+  context '#generate' do
+    it 'generate new code' do
+      code.generate
+      expect(code.get).not_to eq('1111')
+    end
+  end
+end
