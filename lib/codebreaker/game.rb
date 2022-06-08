ee392b08d7bf0249f85221a0c83e5aebb6201ae4
Vasyl <vasyl.laba@rubygarage.org>
2022-06-08 17:12:16 +0300

    Seve user stats when attemp gone

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -69,8 +69,7 @@ module Codebreaker
     private
 
     def save_stats(result)
-      return unless result[0] == '++++ (win)'
-      return unless @attempts.positive?
+      return unless result[0] == '++++ (win)' || @attempts.positive?
 
       @player.attempts_used = (@attempts)
       @player.hints_used = (@hints)
