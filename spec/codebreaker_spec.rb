a6d8a2ccadfcf24e1cc3b35e5b5d7288999e6f0b
Vasyl <vasyl.laba@rubygarage.org>
2022-06-09 10:34:07 +0300

    Refactor tests because separate it

--- spec/codebreaker_spec.rb
+++ spec/codebreaker_spec.rb
@@ -8,24 +8,27 @@ RSpec.describe 'Game' do
     game = Codebreaker::Game.new
     expect(game).to be_instance_of(Codebreaker::Game)
   end
+
   context '#play' do
     let(:game) { Codebreaker::Game.new }
-    it 'Codebreaker game test play method' do
-      play_test('6543', '5643', '++--')
-      play_test('6543', '6411', '+-')
-      play_test('6543', '6544', '+++')
-      play_test('6543', '3456', '----')
-      play_test('6543', '6666', '+')
-      play_test('6543', '2666', '-')
-      play_test('6543', '2222', '')
-      play_test('6666', '1661', '++')
-      play_test('1234', '3124', '+---')
-      play_test('1234', '1524', '++-')
-      play_test('1234', '1234', '++++ (win)')
-    end
-    def play_test(user_code, secret_code, result)
-      game.new_game('Mykola', :medium)
-      expect(game.play(user_code, secret_code)).to eq(result)
+
+    all_cases = [['6543', '5643', '++--'],
+                 ['6543', '6411', '+-'],
+                 ['6543', '6544', '+++'],
+                 ['6543', '3456', '----'],
+                 ['6543', '6666', '+'],
+                 ['6543', '2666', '-'],
+                 ['6543', '2222', ''],
+                 ['6666', '1661', '++'],
+                 ['1234', '3124', '+---'],
+                 ['1234', '1524', '++-'],
+                 ['1234', '1234', '++++ (win)']]
+
+    all_cases.each do |el|
+      it "when user code is #{el[0]} and secret is #{el[1]} result is #{el[2]}" do
+        game.new_game('Mykola', :medium)
+        expect(game.play(el[0], el[1])).to eq(el[2])
+      end
     end
   end
 end
