958d572cd89670e35c25124b95b168d4be981996
Vasyl <vasyl.laba@rubygarage.org>
2022-06-10 11:38:37 +0300

    Refactor difficulty test

--- spec/difficulty_spec.rb
+++ spec/difficulty_spec.rb
@@ -5,17 +5,18 @@ require 'codebreaker/game'
 RSpec.describe 'Game' do
   include Codebreaker
   context '#difficulty_level' do
-    it 'Codebreaker game test difficulty_level method' do
-      diff_test(:easy, 15, 2)
-      diff_test(:medium, 10, 1)
-      diff_test(:hell, 5, 1)
-    end
+    let(:game) { Codebreaker::Game.new }
 
-    def diff_test(diff, attempts, hints)
-      game = Codebreaker::Game.new
-      game.new_game('Tester', diff)
-      expect(game.attempts).to eq(attempts)
-      expect(game.hints).to eq(hints)
+    all_cases = [[:easy, 15, 2],
+                 [:medium, 10, 1],
+                 [:hell, 5, 1]]
+
+    all_cases.each do |difficulty, attempts, hints|
+      it "when difficulty is #{difficulty} you have #{attempts} attempts and #{hints} hints" do
+        game.new_game('Tester', difficulty)
+        expect(game.attempts).to eq(attempts)
+        expect(game.hints).to eq(hints)
+      end
     end
   end
 end
