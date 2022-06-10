7c514d6bcb1ca7dbfef03b487a9d323eecae892d
Vasyl <vasyl.laba@rubygarage.org>
2022-06-10 11:45:18 +0300

    Refactor rename methot valid? to code_valid?

--- lib/codebreaker/validate_service.rb
+++ lib/codebreaker/validate_service.rb
@@ -3,7 +3,7 @@
 module Codebreaker
   # validation method for codebreaker
   module ValidateService
-    def self.valid?(code)
+    def self.code_valid?(code)
       return true if code =~ /^[1-6]{#{::Codebreaker::SecretCodeGenerator.const_get(:CODE_SIZE)}}$/
 
       false
--- spec/validate_service_spec.rb
+++ spec/validate_service_spec.rb
@@ -5,10 +5,10 @@ require 'codebreaker/game'
 RSpec.describe 'ValidateService' do
   context '.valid?' do
     it 'Validate service test valid? false' do
-      expect(Codebreaker::ValidateService.valid?('1237')).to eq(false)
+      expect(Codebreaker::ValidateService.code_valid?('1237')).to eq(false)
     end
     it 'Validate service test valid? true' do
-      expect(Codebreaker::ValidateService.valid?('1336')).to eq(true)
+      expect(Codebreaker::ValidateService.code_valid?('1336')).to eq(true)
     end
   end
   context '.name_valid?' do
