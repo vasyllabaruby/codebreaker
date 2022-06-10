654b0e8e4367a3eb7c08c4e7ad0ff64134466d89
Vasyl <vasyl.laba@rubygarage.org>
2022-06-10 11:31:45 +0300

    Update tests for hinst

--- spec/hint_spec.rb
+++ spec/hint_spec.rb
@@ -4,10 +4,16 @@ RSpec.describe 'Game' do
   include Codebreaker
   context '#hint' do
     let(:game) { Codebreaker::Game.new }
-    it 'Codebreaker game test hint method' do
+    it 'you have not hints true' do
       expect(game.hint).to eq('you have no hints')
+    end
+    it 'you have not hints false' do
       game.new_game('tester', :easy)
       expect(game.hint).not_to eq('you have no hints')
+    end
+    it 'hint is one of secret code numbers' do
+      game.new_game('tester', :easy)
+      expect(game.instance_variable_get(:@secret_code).chars).to include(game.hint)
     end
   end
 end
