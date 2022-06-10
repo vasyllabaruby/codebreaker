79709941fffffdbe6ddb9668bc0a0a0233f9e113
Vasyl <vasyl.laba@rubygarage.org>
2022-06-10 16:33:37 +0300

    Refactor tests because circle ci

--- spec/codebreaker_spec.rb
+++ spec/codebreaker_spec.rb
@@ -10,8 +10,6 @@ RSpec.describe 'Game' do
   end
 
   context '#play' do
-    let(:game) { Codebreaker::Game.new }
-
     all_cases = [['6543', '5643', '++--'], ['6543', '6411', '+-'], ['6543', '6544', '+++'],
                  ['6543', '3456', '----'], ['6543', '6666', '+'], ['6543', '2666', '-'],
                  ['6543', '2222', ''], ['6666', '1661', '++'], ['1234', '3124', '+---'],
@@ -19,6 +17,7 @@ RSpec.describe 'Game' do
 
     all_cases.each do |user_code, secret_code, result|
       it "when user code is #{user_code} and secret is #{secret_code} result is #{result}" do
+        game = Codebreaker::Game.new
         game.new_game('Mykola', :medium)
         game.instance_variable_set(:@secret_code, secret_code)
         expect(game.play(user_code)).to eq(result)
