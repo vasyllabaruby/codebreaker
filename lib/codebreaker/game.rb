ef59240d3b426b556aa65b530339cf9befceda0e
Vasyl <vasyl.laba@rubygarage.org>
2022-06-21 11:36:01 +0300

    Refactor difficulty_check method

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -52,7 +52,7 @@ module Codebreaker
 
     def difficulty_check(difficulty)
       diff = %i[easy medium hell]
-      return false unless diff.include?(difficulty.to_sym.downcase)
+      return false unless diff.include?(difficulty)
 
       true
     end
