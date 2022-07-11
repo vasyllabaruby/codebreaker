2f58027efbe6cb10500dd1f76cca8f4eb72bbc97
Vasyl <vasyl.laba@rubygarage.org>
2022-07-11 23:36:07 +0300

    Separate win? and attempts_gone? methods

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -36,7 +36,8 @@ module Codebreaker
 
       @result = check(user_code.chars, @secret_code.chars)
       @attempts -= 1
-      save_stats if end_game?
+      save_stats if win?
+      end_game if attempts_gone?
       @result
     end
 
@@ -70,9 +71,14 @@ module Codebreaker
       @hints_list = @secret_code.chars.sample(@hints)
     end
 
-    def end_game?
+    def win?
       return true if result == '++++'
-      return true unless @attempts > 1
+
+      false
+    end
+
+    def attempts_gone?
+      return true unless @attempts.positive?
 
       false
     end
@@ -81,13 +87,18 @@ module Codebreaker
       @statistic = load || []
     end
 
+    def end_game
+      save_stats
+      @result = @secret_code
+    end
+
     def save_stats
       @player.update_attempts_used(@attempts)
       @player.update_hints_used(@hints)
       @player.time = (DateTime.now.strftime('%Y.%m.%d - %T'))
       @statistic.append(@player)
       save(@statistic)
-      @secret_code
+      @result
     end
 
     def generate_secret_code
--- lib/codebreaker/version.rb
+++ lib/codebreaker/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Codebreaker
-  VERSION = '3.0.2'
+  VERSION = '3.0.4'
 end
